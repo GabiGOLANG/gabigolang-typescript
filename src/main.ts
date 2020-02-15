8f5bf941bb20434068fc246a4462ecf1c0567d38
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-02-15 18:41:26 -0300

    fix: import right file

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import { readFileSync } from "fs";
+import { readFileSync } from "fs"
 
-import Parser from "./parser/Parser";
-import Lexer from "./lexer/Lexer";
-import Interpreter from "./interpreter/Interpreter";
-import Resolver from "./resolver/Resolver";
+import Parser from "./parser/Parser"
+import Lexer from "./lexer/Lexer"
+import Interpreter from "./interpreter/Interpreter"
+import Resolver from "./resolver/Resolver"
 
 function main(): void {
-  const source = readFileSync("./main.yajs", "utf8");
+  const source = readFileSync("./main.gabigolang", "utf8")
 
-  const lexer = new Lexer(source);
-  const parser = new Parser(lexer.lex());
-  const interpreter = new Interpreter();
-  const resolver = new Resolver(interpreter);
+  const lexer = new Lexer(source)
+  const parser = new Parser(lexer.lex())
+  const interpreter = new Interpreter()
+  const resolver = new Resolver(interpreter)
 
-  const { ok, statements } = parser.parse();
+  const { ok, statements } = parser.parse()
 
   if (ok) {
-    resolver.resolve(statements);
-    interpreter.interpret(statements);
+    resolver.resolve(statements)
+    interpreter.interpret(statements)
   }
 }
-main();
+main()
